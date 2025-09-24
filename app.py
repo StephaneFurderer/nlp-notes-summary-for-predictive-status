a481a49a68d20b73627b431864d45ead05eec10c
Steph Furderer <sfurderer@csatp.com>
2025-09-24 11:20:31 -0700

    clean UI

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+#create main page and subpages under pages folder
+st.set_page_config(page_title="Micro-Level Reserving Framework", layout="wide")
+st.title("Micro-Level Reserving Framework")
+
+    
+    
